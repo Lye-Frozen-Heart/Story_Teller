96a914185b9bee2502a3b42023feff93b6eb9a24
Lye-Frozen-Heart <torresbravoalex@gmail.com>
2024-02-20 16:46:36 +0100

    Entered new libraries

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "../components/Header/Header";
+import ReactPlayer from "react-player";
+import { Card, CardTitle, Row, Col, CardBody } from "reactstrap";
+import ReactCanvasPaint from "react-canvas-paint";
+import "react-canvas-paint/dist/index.css";
 
 function Home() {
   return (
     <>
       <Header></Header>
+      <Row style={{ display: "flex", justifyContent: "center", padding: "2%" }}>
+        <Col>
+          <ReactCanvasPaint
+            colors={[
+              "#7030A2",
+              "#000000",
+              "#0170C1",
+              "#FE0002",
+              "#FFFF01",
+              "#FFFFFF",
+            ]}
+          />
+        </Col>
+      </Row>
     </>
   );
 }
