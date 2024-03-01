42fbbc5dbbbfe51487780ff03b5133fd10db3ec0
Núria Ruiz Font <nuriaruizfont@gmail.com>
2024-03-01 19:33:23 +0100

    add finished footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
 
+import { Footer } from "./components";
 import Home from "./pages/Home";
 import LevelDisplayer from "./pages/LevelDisplayer/LevelDisplayer";
 import AnimalSounds from "./pages/levels/AnimalSounds/AnimalSounds";
@@ -9,36 +10,52 @@ import { hawk2, howl2, oink1mp3 } from "../src/assets/sounds";
 import MemoryGame from "./pages/levels/MemoryGame/MemoryGame";
 import { Header } from "./components";
 function App() {
-  const answerPig = 'pig', answerHawk = 'hawk', answerWolf = 'wolf';
-  const redirectPig = '/animal_w', redirecWolf = '/animal_h',redirectHawk = '/memory';
+  const answerPig = "pig",
+    answerHawk = "hawk",
+    answerWolf = "wolf";
+  const redirectPig = "/animal_w",
+    redirecWolf = "/animal_h",
+    redirectHawk = "/memory";
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" Component={Home}/>
-        <Route path="/levels" Component={LevelDisplayer}/>
+        <Route path="/" Component={Home} />
+        <Route path="/levels" Component={LevelDisplayer} />
         <Route
           path="/animal_p"
           Component={() => (
-            <AnimalSounds sounds={oink1mp3} correctOption={answerPig} redirect={redirectPig}/>
+            <AnimalSounds
+              sounds={oink1mp3}
+              correctOption={answerPig}
+              redirect={redirectPig}
+            />
           )}
         />
         <Route
           path="/animal_w"
           Component={() => (
-            <AnimalSounds sounds={howl2} correctOption={answerWolf} redirect={redirecWolf} />
+            <AnimalSounds
+              sounds={howl2}
+              correctOption={answerWolf}
+              redirect={redirecWolf}
+            />
           )}
         />
         <Route
           path="/animal_h"
           Component={() => (
-            <AnimalSounds sounds={hawk2} correctOption={answerHawk} redirect={redirectHawk} />
+            <AnimalSounds
+              sounds={hawk2}
+              correctOption={answerHawk}
+              redirect={redirectHawk}
+            />
           )}
         />
         <Route path="/memory" Component={MemoryGame}></Route>
       </Routes>
+      <Footer/>
     </>
   );
 }
-
 export default App;
